b6fcb5fa8e22ca87415869a84d0897ca0329618e
lawrencejr <oputalawrence@gmail.com>
2024-12-31 10:50:31 +0100

    switched to server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 // start server
-startServer(`mongodb://127.0.0.1/rtk`);
+startServer(`mongodb://127.0.0.1/`);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
