971c16f0b266ee6a7225ccf43cc707640d4bf9f3
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2025-01-08 21:53:23 +0100

    Dashboard ui fix

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -5,14 +5,13 @@ const Sidebar = () => {
     <section className="hidden sm:flex flex-col justify-between h-screen sticky min-w-[14rem] max-w-[15rem] p-5 bg-[#fefefe] border-r border-gray-200 top-0 left-0">
       <div>
         <Link to={`/`}>
-          <p className="font-bold text-lg mb-6 text-blue-600">Infomart</p>
+          <p className="font-bold text-lg mb-8 text-blue-600">Infomart</p>
         </Link>
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-6">
           <nav>Overview</nav>
           <nav>Courses</nav>
           <nav>Marketplace</nav>
           <nav>Add Course</nav>
-          <nav>Settings</nav>
         </div>
       </div>
       <button className="bg-red-500 text-white font-semibold w-full pt-1 pb-2 rounded-sm">
