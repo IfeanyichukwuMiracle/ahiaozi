1cf6c11ef1fbfabfc35cea06543f6cef73ef81d3
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2025-01-11 11:23:34 +0100

    backend url update

--- client/src/backend-url.js
+++ client/src/backend-url.js
@@ -1,2 +1,3 @@
-const backend_url = "http://localhost:5000/api/v1";
+// const backend_url = "http://localhost:5000/api/v1";
+const backend_url = "https://ahiaozi.onrender.com/api/v1";
 export default backend_url;
