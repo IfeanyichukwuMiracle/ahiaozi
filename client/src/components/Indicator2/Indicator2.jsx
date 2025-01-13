9f13f27d44ed5f4a344f2cf7bc8115322bed2db7
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2025-01-13 19:36:06 +0100

    indicator added

--- client/src/components/Indicator2/Indicator2.jsx
+++ client/src/components/Indicator2/Indicator2.jsx
@@ -1,10 +1,11 @@
+import {motion} from "motion/react"
 
 const Indicator2 = () => {
   return (
 <section>
-<div className="bg-red-200 size-4 rounded-full flex items-center justify-center" >
+<motion.div animate={{opacity:[0.3,1,0.3]}} transition={{duration:1.5,repeat:Infinity}} className="bg-red-200 size-4 rounded-full flex items-center justify-center" >
    <div className="bg-red-600 rounded-full size-2" >
-   </div>
+   </motion.div>
 </div>
 </section>
   )
