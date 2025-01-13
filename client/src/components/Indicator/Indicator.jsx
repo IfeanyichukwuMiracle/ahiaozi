586851f99e82061f1f2beca13d923d2306c82a7e
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2025-01-13 19:33:05 +0100

    indicator added

--- client/src/components/Indicator/Indicator.jsx
+++ client/src/components/Indicator/Indicator.jsx
@@ -4,7 +4,7 @@ import {motion} from "motion/react"
 const Indicator = () => {
   return (
 <section className={`absolute -top-[.4rem] -right-[.4rem]`}>
-<motion.div animate={{opacity:[0,1,0.7]}} transition={{duration:1.5,repeat:Infinity}} className="bg-red-200 size-4 rounded-full flex items-center justify-center" >
+<motion.div animate={{opacity:[0.5,1]}} transition={{duration:1.5,repeat:Infinity}} className="bg-red-200 size-4 rounded-full flex items-center justify-center" >
    <div className="bg-red-600 rounded-full size-2" >
    </div>
 </motion.div>
