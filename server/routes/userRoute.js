c7e2902094cce282cfad604b359d7100ba2271e2
Ifeanyichukwu Miracle <ifeanyimiraclechuks@gmail.com>
2025-01-17 10:24:52 +0100

    preparing for merge

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const { signup, login } = require("../controllers/userController");
-
-// signup
-router.post("/signup", signup);
-// login
-router.post("/login", login);
-
-module.exports = router;
--- server/utils/AppError.js
+++
@@ -1,9 +0,0 @@
-class AppError extends Error {
-  constructor(message, code, type) {
-    super(message);
-    this.code = code || 500;
-    this.type = type;
-  }
-}
-
-module.exports = AppError;
