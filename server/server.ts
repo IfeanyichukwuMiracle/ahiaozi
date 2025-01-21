4222f2d47bd827fbecd5b5697101d3f6f9c6dde8
lawrencejr <oputalawrence@gmail.com>
2025-01-21 07:24:07 +0100

    basic widhlist functions

--- server/server.ts
+++ server/server.ts
@@ -16,6 +16,7 @@ import UserRouter from "./routes/user";
 import CartRouter from "./routes/cart";
 import SectionRouter from "./routes/sections";
 import LessonRouter from "./routes/lessons";
+import WishListRouter from "./routes/wishlist";
 
 // Using json middleware
 app.use(express.json());
@@ -32,6 +33,7 @@ app.use("/api/v0/courses/", CourseRouter);
 app.use("/api/v0/cart/", CartRouter);
 app.use("/api/v0/sections/", SectionRouter);
 app.use("/api/v0/lessons/", LessonRouter);
+app.use("/api/v0/wishlists/", WishListRouter);
 
 // If route is not found
 app.use(notFound);
