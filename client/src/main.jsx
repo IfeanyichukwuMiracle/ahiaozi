43e8243004e0c6230cfe130eafe0aa1107b19a9c
IfeanyichukwuMiracle <111650485+IfeanyichukwuMiracle@users.noreply.github.com>
2025-02-06 09:58:45 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+//import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.jsx";
@@ -7,9 +7,9 @@ import "./index.css";
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ // <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
-  </StrictMode>
+//  </StrictMode>
 );
